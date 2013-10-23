e23afda3d4352a0c3a6d8dd1b4382936ee79e932
András Mamenyák <mamenyaka@gmail.com>
2013-10-23 16:08:43 +0200

    add constants

--- neural.cpp
+++ neural.cpp
@@ -30,32 +30,30 @@ public:
 class Neuron
 {
 public:
-  int id;
-  double value;
-  double bias;
-  double delta;
-  Dendrite *dendrites;
-
-  Neuron(int id = 0, double value = 0.0, double bias = 0.0) : id(id), value(value), bias(bias)
+  Neuron(const int id = 0, const double value = 0.0, const double bias = 0.0) : id(id), value(value), bias(bias)
   {
     delta = 0.0;
   }
 
   ~Neuron()
   {
-    delete[] dendrites;
+    delete[] dendrite;
   }
 
   /*
-   * Set the dendrites from the neuron to given dendrite
+   * Set the dendrite from the neuron to given dendrite
    */
-  void set_dendrites(int n)
+  void set_dendrite(const int n)
   {
-    dendrites = new Dendrite[n];
+    dendrite = new Dendrite[n];
 
     for (int i = 0; i < n; i++)  // Initialize the dendrite to attach to next layer
-      dendrites[i].points_to = i;
+      dendrite[i].points_to = i;
   }
+
+  int id;
+  double value, bias, delta;
+  Dendrite *dendrite;
 };
 
 class Layer
@@ -69,17 +67,17 @@ public:
     delete[] neuron;
   }
 
-  void initialize(int size)
+  void initialize(const int size)
   {
     neuron = new Neuron[size];
   }
 
-  Neuron get_neuron(int index)
+  Neuron get_neuron(const int index) const
   {
     return neuron[index];
   }
 
-  void set_neuron(Neuron neuron, int index)
+  void set_neuron(Neuron neuron, const int index)
   {
     this->neuron[index] = neuron;
   }
@@ -96,20 +94,14 @@ public:
   }
 
   ~Network()
-  {
-    delete[] inputs;
-    delete[] outputs;
-  }
+  {}
 
   /*
    * Set various parameters of the net
    */
-  void set_data(double learning_rate, int layer[])
+  void set_data(const double learning_rate, const int layer[])
   {
     this->learning_rate = learning_rate;
-
-    inputs = new double[layer[0]];
-    outputs = new double[layer[2]];
 
     for (int i = 0; i < 3; i++)
     {
@@ -120,7 +112,10 @@ public:
     randomize();
   }
 
-  void set_input_output(double input[], double output[])
+  /*
+   * The real test
+   */
+  void set_input_output(const double input[], double output[])
   {
     for (int i = 0; i < neuron_per_layer[0]; i++)
       layer[0].neuron[i].value = input[i];
@@ -142,7 +137,7 @@ public:
    *
    * Weight += LearningRate * Delta * Input
    */
-  void train(double input[], double output[])
+  void train(const double input[], const double output[])
   {
     double Actual, Delta;
 
@@ -163,14 +158,14 @@ public:
         }
         else  // Hidden layer
         {
-          Delta = Actual * (1.0 - Actual) * sum_weight_delta(i, j);
+          Delta = Actual * (1.0 - Actual) * sum_weight_delta(i);
         }
 
         if (i > 0)  // Input layer does not have a bias
           layer[i].neuron[j].bias += Delta*learning_rate;
 
         for (int k = 0; k < neuron_per_layer[i-1]; k++)  // Calculate the new weights
-          layer[i-1].neuron[k].dendrites[j].weight +=  Delta*learning_rate*layer[i-1].neuron[k].value;
+          layer[i-1].neuron[k].dendrite[j].weight +=  Delta*learning_rate*layer[i-1].neuron[k].value;
       }
   }
 
@@ -185,16 +180,14 @@ private:
       {
         if (i < 2)  // Last layer does not require weights
         {
-          layer[i].neuron[j].set_dendrites(neuron_per_layer[i+1]); // Initialize the dendites
+          layer[i].neuron[j].set_dendrite(neuron_per_layer[i+1]); // Initialize the dendites
 
           for (int k = 0; k < neuron_per_layer[i+1]; k++)
-            layer[i].neuron[j].dendrites[k].weight = get_rand(); // Let weight be the random value
+            layer[i].neuron[j].dendrite[k].weight = get_rand(); // Let weight be the random value
         }
 
         if (i > 0)  // First layer does not need biases
-        {
           layer[i].neuron[j].bias = get_rand();
-        }
       }
   }
 
@@ -207,7 +200,7 @@ private:
       for (int j = 0; j < neuron_per_layer[i]; j++)
       {
         for (int k = 0; k < neuron_per_layer[i-1]; k++)  // Multiply and add all the inputs
-          layer[i].neuron[j].value += layer[i-1].neuron[k].value*layer[i-1].neuron[k].dendrites[j].weight;
+          layer[i].neuron[j].value += layer[i-1].neuron[k].value*layer[i-1].neuron[k].dendrite[j].weight;
 
         layer[i].neuron[j].value += layer[i].neuron[j].bias;  // Add bias
         layer[i].neuron[j].value = limiter(layer[i].neuron[j].value);  // Squash that value
@@ -217,12 +210,12 @@ private:
   /*
    * Sigmoid activation function
    */
-  double limiter(double x)
+  double limiter(const double x) const
   {
     return 1.0/(1.0 + exp(-x));
   }
 
-  double get_rand()
+  double get_rand() const
   {
     return -1.0 + ((double) rand()/RAND_MAX)*2.0;
   }
@@ -230,47 +223,47 @@ private:
   /*
    * Calculate sum of weights * delta. Used in back prop.
    */
-  double sum_weight_delta(int Nlayer, int Nneuron)
+  double sum_weight_delta(const int Nlayer) const
   {
     double result = 0.0;
 
     for (int i = 0; i < neuron_per_layer[Nlayer+1]; i++)  // Go through all the neuron in the next layer
-      result += layer[Nlayer].neuron[Nlayer].dendrites[i].weight*layer[Nlayer+1].neuron[i].delta;
+      result += layer[Nlayer].neuron[Nlayer].dendrite[i].weight*layer[Nlayer+1].neuron[i].delta;
 
     return result;
   }
 
   double learning_rate;
   Layer layer[3];
-  double *inputs;
-  double *outputs;
   int neuron_per_layer[3];
 };
 
 int main()
 {
-  int layer[3] = {11, 11, 1};  // input, hidden, output
-  Network network;
+  const int Ninput = 4;
+  const int Nneuron = 11;
+  const int layer[3] = {Nneuron, Nneuron, 1};  // input, hidden, output
 
+  Network network;
   network.set_data(0.1, layer);
+
 
   std::cout << "Start training.\n\n";
 
-  // XOR
-  int iter = 0;
-  double tr_inp[4][11] = {
-      {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0},
-      {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
-      {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0},
-      {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0}
+  const double tr_inp[Ninput][Nneuron] = {
+    {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0}
   };
-  double tr_out[4][1] = { {1.0}, {0.0}, {0.0}, {0.0} };
+  const double tr_out[Ninput][1] = { {1.0}, {0.0}, {0.0}, {0.0} };
 
+  int iter = 0;
   std::cout << "Enter number of training Iterations: ";
   std::cin >> iter;
 
   for(int i = 0; i < iter; i++)
-    for(int j = 0; j < 4; j++)
+    for(int j = 0; j < Ninput; j++)
       network.train(tr_inp[j], tr_out[j]);
 
   std::cout << "\nEnd training.";
@@ -278,9 +271,9 @@ int main()
 
   std::cout << "\n\nStart testing.";
 
-  double output[4];
+  double output[Ninput];
 
-  for(int j = 0; j < 4; j++)
+  for(int j = 0; j < Ninput; j++)
   {
     std::cout << "\n\nCase number: " << j+1;
 
